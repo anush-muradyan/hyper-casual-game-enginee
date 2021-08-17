a29c96cfc2c404125c4cf4816fc29a47b1b8fd88
Anush Muradyan <>
2021-08-17 15:05:17 +0400

    example pull request

---
+++ Assets/Scripts/Entity/Obstacles.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace Entity
+{
+    public class Obstacles : MonoBehaviour
+    {
+        [SerializeField] private float speed = 4f;
+
+        private void Start()
+        {
+            //TODO:  Remove if dont need it, only example for pull request
+        }
+    }
+}
