1f7c176286ae8516a2d8d4ef0f1b493566632c15
Arman Karapetyan <armankarapetyan.adana@gmail.com>
2021-09-02 19:50:03 +0400

    merge fix complete

--- Assets/Scripts/Core/MVVM/Controls/TextStaticComponent.cs
+++ Assets/Scripts/Core/MVVM/Controls/TextStaticComponent.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 namespace Core.MVVM.Controls {
 	public class TextStaticComponent : StaticComponentControl<Text> {
 		public override void SetValue(object value) {
+			if (value == null) {
+				return;
+			}
 			var str = value.ToString();
 			Property.text = str;
 		}
--- Assets/Scripts/Core/MVVM/Test.cs
+++ Assets/Scripts/Core/MVVM/Test.cs
@@ -2,18 +2,15 @@ using Core.MVVM.Home;
 using Core.MVVM.Image;
 using UnityEngine;
 
-namespace Core.MVVM
-{
-	public class Test : MonoBehaviour
-	{
+namespace Core.MVVM {
+	public class Test : MonoBehaviour {
 		[SerializeField] private HomeView homeView;
 		[SerializeField] private ImageView imageView;
 
 
-		private void Start()
-		{
-			imageView.Init(new ImageModel {});
-
+		private void Start() {
+			homeView.Init(new HomeModel());
+			imageView.Init(new ImageModel());
 		}
 	}
 }
